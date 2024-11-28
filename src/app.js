60328f577952854845386c365387052c77408950
TSMJosem <receptorx89@gmail.com>
2024-11-28 14:59:37 -0600

    Crear variables e imprimir suma

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 console.log('Hello, World!');
 console.log('Prueba de Cambio');
 console.log('Prueba de Cambio Exitosa'); 
+
+const numero = 2;
+const numero2 = 50;
+
+console.log(numero + numero2);
